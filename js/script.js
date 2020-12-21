de7781c2bfaace6aa056565b195cda999a0a66d4
Alex Sircu <alex.sricu@gmail.com>
2020-12-21 15:50:39 +0100

    Milestone 2 - stelle

--- js/script.js
+++ js/script.js
@@ -3,23 +3,30 @@ var app = new Vue({
   data: {
     filmToSearch: "",
     prefixOfFilmUrl: "https://image.tmdb.org/t/p/w220_and_h330_face/",
-    filmsArray: []
+    filmsArray: [],
+    averageVoteArray: []
 
   },
   methods: {
     searchFilm: function() {
-      var myThis = this;
+      var self = this;
       axios
         .get('https://api.themoviedb.org/3/search/movie', {
           params: {
             api_key: '5f1d9b533544f75b3d29837feba9a687',
-            query: myThis.filmToSearch
+            query: self.filmToSearch
           }
         })
         .then(function (response) {
-          myThis.filmsArray = response.data.results;
-          console.log(response.data.results);
+          self.filmsArray = response.data.results;
+          // console.log(response.data.results);
+
+          for (var i = 0; i < self.filmsArray.length; i++) {
+            let averageCeilVote = Math.ceil(self.filmsArray[i].vote_average/2);
+            self.averageVoteArray.push(averageCeilVote)
+          }
         })
+      self.filmToSearch = "";
     }
   }
 });
