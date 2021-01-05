ef1804d98621b8bb2621be26b9fbe2a74e95eaab
Alex Sircu <alex.sricu@gmail.com>
2021-01-05 20:14:30 +0100

    Tentativo milestone 4

--- js/script.js
+++ js/script.js
@@ -7,7 +7,9 @@ var app = new Vue({
     filmsArray: [],
     seriesArray: [],
     filmsSeriesArray: [],
+    newFilmsSeriesArray: [],
     averageVoteArray: [],
+    castArray: [],
     flagLanguage: ""
 
   },
@@ -21,9 +23,10 @@ var app = new Vue({
             query: self.filmToSearch
           }
         })
-        .then(function(response) {
+        .then( function(response) {
           self.filmsArray = response.data.results;
           self.filmsSeriesArray = self.filmsArray.concat(self.seriesArray);
+          self.filmsSeriesArray.sort(self.orederByPopularity);
           // console.log(response.data.results);
 
           for (var i = 0; i < self.filmsSeriesArray.length; i++) {
@@ -42,9 +45,10 @@ var app = new Vue({
             query: self.filmToSearch
           }
         })
-        .then(function(response) {
+        .then( function(response) {
           self.seriesArray = response.data.results;
           self.filmsSeriesArray = self.seriesArray.concat(self.filmsArray);
+          self.filmsSeriesArray.sort(self.orederByPopularity);
           // console.log(response.data.results[0].name);
 
           for (var i = 0; i < self.filmsSeriesArray.length; i++) {
@@ -54,10 +58,54 @@ var app = new Vue({
           self.toFlagLanguage();
         })
     },
+    searchMovieCast: function() {
+      var self = this;
+      axios
+        .get('https://api.themoviedb.org/3/movie/671/credits', {
+          params: {
+            api_key: '5f1d9b533544f75b3d29837feba9a687',
+          }
+        })
+        .then( function(response) {
+          let castArrayApi = response.data.cast;
+          // console.log(castArrayApi);
+          castArrayApi.forEach((item, i) => {
+            if (self.castArray.length < 5) {
+              self.castArray.push(item.name)
+            }
+          });
+
+          self.newFilmsSeriesArray = self.filmsSeriesArray.map(
+            (element) => {
+              let characters = self.castArray;
+
+              const newFilmObj = {
+                ...element,
+                characters
+              };
+              console.log(newFilmObj);
+              return newFilmObj;
+            }
+          )
+        })
+    },
+    searchSeriesCast: function() {
+      var self = this;
+      axios
+        .get('https://api.themoviedb.org/3/tv/"INSERIRE ID DEL FILM"/credits', {
+          params: {
+            api_key: '5f1d9b533544f75b3d29837feba9a687',
+          }
+        })
+        .then( function(response) {
+
+        })
+    },
     startFunctions: function() {
       this.filmsSeriesArray = [];
       this.searchFilm();
       this.searchTvSeries();
+      this.searchMovieCast();
     },
     toFlagLanguage: function() {
       for (var i = 0; i < this.filmsSeriesArray.length; i++) {
@@ -70,6 +118,9 @@ var app = new Vue({
           this.filmsSeriesArray[i].flagLanguage = "https://upload.wikimedia.org/wikipedia/commons/b/ba/Flag_of_Germany.svg";
         }
       }
+    },
+    orederByPopularity: function (a, b) {
+     return b.popularity - a.popularity;
     }
   }
 });
